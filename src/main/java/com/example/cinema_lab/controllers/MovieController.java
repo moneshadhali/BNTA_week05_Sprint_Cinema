6095e9cc8cb39e7006aa6e4350bc516b9cba7c7e
Monesha <moneshadhali@gmail.com>
2024-02-27 15:47:19 +0000

    movie controller added

---
+++ src/main/java/com/example/cinema_lab/controllers/MovieController.java
@@ -0,0 +1,28 @@
+package com.example.cinema_lab.controllers;
+
+import com.example.cinema_lab.models.Movie;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieController {
+
+    @GetMapping
+    public ResponseEntity<String> getMovie(){
+
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<String> getMovieById(@PathVariable long id){
+
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postMovie(Movie movie){
+        return new ResponseEntity<>("", HttpStatus.CREATED);
+    }
+}
