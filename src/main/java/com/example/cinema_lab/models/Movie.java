27c0bb7a9390c730a3e550eb8368f7b6c7694a76
Monesha <moneshadhali@gmail.com>
2024-02-27 15:35:05 +0000

    movies model created

---
+++ src/main/java/com/example/cinema_lab/models/Movie.java
@@ -0,0 +1,55 @@
+package com.example.cinema_lab.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "rating")
+    private String rating;
+    @Column(name = "duration")
+    private double duration;
+
+    public Movie( String title, String rating, double duration) {
+        this.title = title;
+        this.rating = rating;
+        this.duration = duration;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+}
